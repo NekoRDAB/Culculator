ed7cc89ef5196cf5a4abf8e6faa527df7511d1f4
Danil <ferrum268@mail.ru>
2023-10-27 06:36:55 +0500

    update gitignore

--- Culculator/obj/Debug/net6.0/Culculator.AssemblyInfo.cs
+++ Culculator/obj/Debug/net6.0/Culculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Culculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c1f71f0b784e4bdba204a205155266b5199834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Culculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Culculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
