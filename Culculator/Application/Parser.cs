b6debfaccd7c07e20077a54b665da2803a3a34a8
hempi <115639626+VladislavBukirev@users.noreply.github.com>
2023-11-03 13:26:28 +0500

    GetIngredientFromDB

--- Culculator/Application/Parser.cs
+++ Culculator/Application/Parser.cs
@@ -1,5 +1,21 @@
 using System;
+using Culculator.DataBase;
+using Culculator.Domain;
 
 
 public class Parser
-{ }
+{
+    public Ingredient GetIngredientFromDB(string ingredientName)
+    {
+        using (var ingredientsDB = new IngredientsContext())
+        {
+            var ingredient = ingredientsDB
+                .IngredientsDataBase
+                .FirstOrDefault(i => i.Name == ingredientName);
+
+            if (ingredient == null)
+                throw new KeyNotFoundException($"Ингредиент с названием {ingredientName} не найден");
+            return ingredient; 
+        }
+    }
+}
--- Culculator/Program.cs
+++ Culculator/Program.cs
@@ -10,23 +10,28 @@ namespace Culculator
     {
         static void Main(string[] args)
         {
-            var egg = new Ingredient("Яйцо", 7);
-            var sugar = new Ingredient("Сахар", 0.06);
-            using (var db = new IngredientsContext())
-            {
-                db.IngredientsDataBase.Add(egg);
-                db.IngredientsDataBase.Add(sugar);
-                db.SaveChanges();
-                // db.IngredientsDB.ExecuteDelete();
-            }
-            var friedEggRecipe =
-                new Recipe("Яичница", "Простые",
-                    "Яйцо 3; Сосиска 2; Помидор 200", 3, "Вкусная яишенка");
-            using (var recipeDB = new RecipesContext())
-            {
-                recipeDB.RecipesDataBase.Add(friedEggRecipe);
-                recipeDB.SaveChanges();
-            }
+            var parserInstance = new Parser();
+            var egg = parserInstance.GetIngredientFromDB("Яйцо");
+            Console.WriteLine(egg.GetType());
+            Console.WriteLine($"{egg.id}, {egg.Name}, {egg.Price}");
+            var nothing = parserInstance.GetIngredientFromDB("Абракадабра");
+            // var egg = new Ingredient("Яйцо", 7);
+            // var sugar = new Ingredient("Сахар", 0.06);
+            // using (var db = new IngredientsContext())
+            // {
+            //     db.IngredientsDataBase.Add(egg);
+            //     db.IngredientsDataBase.Add(sugar);
+            //     db.SaveChanges();
+            //     // db.IngredientsDB.ExecuteDelete();
+            // }
+            // var friedEggRecipe =
+            //     new Recipe("Яичница", "Простые",
+            //         "Яйцо 3; Сосиска 2; Помидор 200", 3, "Вкусная яишенка");
+            // using (var recipeDB = new RecipesContext())
+            // {
+            //     recipeDB.RecipesDataBase.Add(friedEggRecipe);
+            //     recipeDB.SaveChanges();
+            // }
         }
     }
 }
